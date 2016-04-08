463ead89585cce02bde3a775698c6c1da9921113
konradjniemiec <konradjniemiec@gmail.com>
2016-04-08 00:15:09 +0000

    Added Responses!

---
+++ Project1/HttpRequest.h
@@ -0,0 +1,14 @@
+#ifndef HTTP_REQUEST_H
+#define HTTP_REQUEST_H
+
+
+class HttpRequest
+{
+
+};
+
+
+
+
+
+#endif
---
+++ Project1/HttpResponse.h
@@ -0,0 +1,15 @@
+#ifndef HTTP_RESPONSE_H
+#define HTTP_RESPONSE_H
+
+
+class HttpResponse
+{
+
+};
+
+
+
+
+
+#endif
+
