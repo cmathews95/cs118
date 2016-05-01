4ecb9f9b047d23d5d24009e36a0659357f457608
Christie Mathews <mathews.christie@gmail.com>
2016-05-01 04:49:05 +0000

    worked on client

--- Project1/client.cpp
+++ Project1/client.cpp
@@ -6,26 +6,77 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
-
 #include <iostream>
 #include <sstream>
+#include <netdb.h>
 
-int
-main()
+char ipstr[INET_ADDRSTRLEN] = {'\0'};
+struct connection_info{
+  std::string hostname;
+  std::string port;
+  std::string object;
+};
+
+connection_info*:struct urlScraper(char* url) {
+  int i = 0;
+  connection_info* temp = new connection_info();
+  while(url[i]!='\0') {
+
+  }
+
+  return temp;
+
+}
+int dns(char* hostname)
 {
+  struct addrinfo hints;
+  struct addrinfo* res;
+
+  // prepare hints
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET; // IPv4
+  hints.ai_socktype = SOCK_STREAM; // TCP
+
+  // get address
+  int status = 0;
+  if ((status = getaddrinfo(hostname, "80", &hints, &res)) != 0) {
+    std::cerr << "getaddrinfo: " << gai_strerror(status) << std::endl;
+    return 0;
+  }
+
+  // std::cout << "IP addresses for " << hostname << ": " << std::endl;
+
+  for(struct addrinfo* p = res; p != 0; p = p->ai_next) {
+    // convert address to IPv4 address
+    struct sockaddr_in* ipv4 = (struct sockaddr_in*)p->ai_addr;
+
+    // convert the IP to a string and print it:
+    inet_ntop(p->ai_family, &(ipv4->sin_addr), ipstr, sizeof(ipstr));
+    // std::cout << "  " << ipstr << std::endl;
+    return ipstr;
+  }
+
+  freeaddrinfo(res); // free the linked list
+
+  return 1;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+  if(argc <= 1) {
+    std::cerr << "Not Enough Arguments...Pass in URL" << std::endl;
+    return 1;
+  }
+
+  std::cout << "Resource URL: " << argv[1] << std::endl;
   // create a socket using TCP IP
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-  // struct sockaddr_in addr;
-  // addr.sin_family = AF_INET;
-  // addr.sin_port = htons(40001);     // short, network byte order
-  // addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  // memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
-  // if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
-  //   perror("bind");
-  //   return 1;
-  // }
-
+  for ( int i = 0; i < argc; i++ ) {
+    connection_info* req_data = urlScraper(argv[i]);
+  }
   struct sockaddr_in serverAddr;
   serverAddr.sin_family = AF_INET;
   serverAddr.sin_port = htons(40000);     // short, network byte order
